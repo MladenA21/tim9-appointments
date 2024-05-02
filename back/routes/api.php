52755fb2597df444791882cec3e6d98d010b53f3
MladenA21 <mladenaleksic21@gmail.com>
2024-05-03 01:07:50 +0200

    welcome api route

--- back/routes/api.php
+++ back/routes/api.php
@@ -17,6 +17,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function() {
+    return response()->json([
+    'message' => "Welcome to Tim 9 reservations api."]);
+});
+
 Route::middleware('auth:sanctum')->group(function() {
     Route::post('logout', [AuthController::class, 'logout']);
 
